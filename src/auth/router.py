63e2b54372901b2a3470dd42a3f379ec8f6e6bb6
ivansantiagojr <ivansantiago.junior@gmail.com>
2024-11-29 15:04:14 -0300

    feat: create auth and users routes

--- src/auth/router.py
+++ src/auth/router.py
@@ -3,9 +3,8 @@ import uuid
 from fastapi_users import FastAPIUsers
 
 from src.security import auth_backend
-
-from .dependencies import get_user_manager
-from .models import User
+from src.users.dependencies import get_user_manager
+from src.users.models import User
 
 fastapi_users = FastAPIUsers[User, uuid.UUID](
     get_user_manager,
