1f76a302bd23d0cda90a312a25e7f3f6539edefa
ivansantiagojr <ivansantiago.junior@gmail.com>
2024-11-30 04:17:41 -0300

    feat: add more info to users table

--- tests/test_users.py
+++ tests/test_users.py
@@ -8,8 +8,11 @@ from src.users.schemas import UserCreate
 
 @pytest.mark.asyncio
 async def test_user_register(client: AsyncClient):
-    user = UserCreate(email='user@example.com', password='string')
+    user = UserCreate(
+        full_name='String String', email='user@example.com', password='string'
+    )
 
     response = await client.post('/auth/register', json=user.model_dump())
 
     assert response.status_code == HTTPStatus.CREATED
+    assert response.json()['email'] == 'user@example.com'
